29ae2e63b691d9d00d4525ed1c873e6da8d5647f
rosepann <mumushua1225@gmail.com>
2024-12-02 11:43:02 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   //twitter: '',
-  github: 'rosepann',
+  //github: 'rosepann',
   //linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
